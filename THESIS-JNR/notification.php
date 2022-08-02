9b2bd2a7d24a4310e4fa1de6e247f0eddaec3249
Hohenheim <jomtiu16@gmail.com>
2022-08-02 12:28:29 +0800

    change

--- THESIS-JNR/notification.php
+++
@@ -1,5 +0,0 @@
-<?php
-require_once 'connect.php';
-$result_notif = $mysqli->query("SELECT * FROM tbl_installmentaccount WHERE statuss = 'down payment'");
-echo $result_notif->num_rows;
-?>
--- THESIS-JNR/signout.php
+++
@@ -1,6 +0,0 @@
-<?php
-    session_start();
-    unset($_SESSION['admin_username']);
-    unset($_SESSION['username']);
-    header("location: login.php");
-?>
